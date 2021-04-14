f3df9225e0555261b8613daaa042fc2af361ed17
xjyplayer <2532917800@qq.com>
2021-04-14 15:44:03 +0800

    cli

--- private_model/task.go
+++ private_model/task.go
@@ -251,6 +251,19 @@ func Save(f *{{$name}}) *{{$name}} {
 	New{{$queryName}}().save(f)
 	return f
 }
+
+func CheckExistBy{{.StructKey}}(m {{.Type}}) bool {
+	cnt := New{{$queryName}}().kWhe{{.StructKey}(m).Count()
+	return cnt > 0
+}
+
+func GetOneBy{{.StructKey}}(m {{.Type}}) *File {
+	return New{{$queryName}}().kWhe{{.StructKey}(m).GetOne()
+}
+
+func GetFirstBy(m string) *File {
+	return New{{$queryName}}().kWhe{{.StructKey}(m).First()
+}
 {{end}}
 `
 }
