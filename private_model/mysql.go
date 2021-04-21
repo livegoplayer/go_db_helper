77bed9aaea200c24aa31250900fd56358293ffde
xjyplayer <2532917800@qq.com>
2021-04-21 17:51:32 +0800

    cli

--- private_model/mysql.go
+++ private_model/mysql.go
@@ -79,6 +79,9 @@ func (mt *MysqlTask) parseField(fileTxt string) (DefineFields, []Index) {
 				} else {
 					field[3] = field[3][begin:]
 				}
+				if field[3] == "-" {
+					field[3] = snakeString(field[1])
+				}
 			} else {
 				continue
 			}
@@ -417,4 +420,25 @@ func ParseIndex(m map[string][]string, fields DefineFields) []Index {
 	}
 
 	return list
+}
+
+func snakeString(s string) string {
+	data := make([]byte, 0, len(s)*2)
+	j := false
+	num := len(s)
+	for i := 0; i < num; i++ {
+		d := s[i]
+		// or通过ASCII码进行大小写的转化
+		// 65-90（A-Z），97-122（a-z）
+		//判断如果字母为大写的A-Z就在前面拼接一个_
+		if i > 0 && d >= 'A' && d <= 'Z' && j {
+			data = append(data, '_')
+		}
+		if d != '_' {
+			j = true
+		}
+		data = append(data, d)
+	}
+	//ToLower把大写字母统一转小写
+	return strings.ToLower(string(data[:]))
 }
